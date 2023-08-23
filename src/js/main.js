39680925aabea34b924b521e21660551b24768b3
Alex <alexprohorov01@gmail.com>
2023-08-23 18:19:00 +0300

    update timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   let modalState = {};
+  let deadline = "2023-10-10";
 
   changeModalState(modalState);
   modals();
@@ -26,4 +28,5 @@ window.addEventListener("DOMContentLoaded", () => {
     "inline-block"
   );
   forms(modalState);
+  timer(".container1", deadline);
 });
